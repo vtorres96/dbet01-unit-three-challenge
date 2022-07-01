60d27c8315d062f637f9d72f903941f6a284ac6e
Victor Torres Moreno <victor.torres_taking@totalexpress.com.br>
2022-07-01 20:19:47 -0300

    adicionando ssl rejectUNauthorized com valor false

--- src/config/connection.js
+++ src/config/connection.js
@@ -5,7 +5,10 @@ const pool = new Pool({
     host: 'ec2-3-222-74-92.compute-1.amazonaws.com',
     database: 'd7tgfd3lkr5lq',
     password: '92ca49ddc62679b20cb1052ba9c12af9c4cac24fa5fe22ecb2e1e69e55bbe295',
-    port: 5432
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 const query = (text, param) => {
