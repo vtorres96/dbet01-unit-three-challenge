b6c519e08d37684f3a9368094d94f31073b19c7a
Victor Torres Moreno <victor.torres_taking@totalexpress.com.br>
2022-07-01 19:08:21 -0300

    adicionando propriedade start no package.json e configurando conexao com o banco de dados do heroku

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'revisao_unidade_tres',
-    password: '03v01t96m',
+    user: 'bdovawkqjscghh',
+    host: 'ec2-3-222-74-92.compute-1.amazonaws.com',
+    database: 'd7tgfd3lkr5lq',
+    password: '92ca49ddc62679b20cb1052ba9c12af9c4cac24fa5fe22ecb2e1e69e55bbe295',
     port: 5432
 });
 
